5a5290b1a1b91565978b8cabc414bdc66b2b2aa5
filipnathanel <nathanel@o2.pl>
2016-07-07 15:07:58 +0200

    add scanning animation

--- app/scripts/scanner.js
+++ app/scripts/scanner.js
@@ -4,9 +4,16 @@ export default class Scanner {
 
 	constructor(scanner){
 		
+		//scanner
 		this.$scanner = utils.getEl(scanner);
 		this.context = this.$scanner.getContext('2d');
+		//scanned
+		this.$scan = utils.getEl('#scan');
+		this.scanContext = this.$scan.getContext('2d');
+		this.$scanButton = utils.getEl('#start_scan');
 		
+		this.imageLoaded = false;
+
 		this.file = {};
 
 		this.initEvents();
@@ -14,38 +21,79 @@ export default class Scanner {
 	}
 
 	initEvents(){
+
 		this.$scanner.addEventListener('imageUpdate', (e) => {this.onImageUpdated(e)});
+		this.$scanButton.addEventListener('click',(e) => {this.onScanClick(e)})
 	}
 
 
 	onImageUpdated(e){
+
 		var file = e.detail.file;
 
-		// this.$scanner.setAttribute('width', file);
+		this.loadFileToScanner(file);
 
+	}
+
+	loadFileToScanner(file){
 		var fr = new FileReader();
-		console.log(fr);
 
 		fr.addEventListener('load', (e) => {
-			console.log(e);
 			var rawImage = e.target.result;
 			var image = new Image();
 			image.src = rawImage;
-			// console.log(image);
 			this.$scanner.width = image.width;
 			this.$scanner.height = image.height;
 			this.context.drawImage(image,0,0);
-			// this.$scanner.toDataURL('image/png');
+			this.imageLoaded = true;
 		});
 
 		fr.readAsDataURL(file);
+	}
+
+	onScanClick(e){
+		if(this.imageLoaded === true){
+			this.scan();
+		}else{
+			console.log('no image present in the scanner');
+		}
+
+	}
+
+	scan(){
+
+		var start = null;
+		var duration = 2000;
+		
+		var self = this; 
+		
+		function scanLoop(timestamp){
+
+			if (!start) start = timestamp;
+			var progress = timestamp - start;
+			// console.log(progress);
+			
+			// console.log(progress/duration);
+			var currentStep = (progress / duration) * self.$scanner.width;
+			currentStep = currentStep > 0 ? currentStep : 1 ; 
+
+			var scanned = self.context.getImageData(0,0, currentStep, self.$scanner.height);
+			console.log(scanned);
+			self.scanContext.putImageData(scanned,0,0);
+
+			if ( progress < duration ){
+				window.requestAnimationFrame(scanLoop);
+			}
+
+		}
+
+		window.requestAnimationFrame(scanLoop);
+
 
 	}
 
 
 	addFile(file){
-
-		console.log(file);
 
 		var addFileEvent = new CustomEvent('imageUpdate', {
 			detail:file
