79e2b61cafeb7191d47193a6bfb7f5f03e7b2c21
filipnathanel <nathanel@o2.pl>
2016-08-01 14:55:26 +0200

    change the viewport util to return dimensions without scrollbars

--- app/scripts/utils/utils.js
+++ app/scripts/utils/utils.js
@@ -48,8 +48,8 @@ export function updateViewportDimensions() {
         d = document,
         e = d.documentElement,
         g = d.getElementsByTagName('body')[0],
-        x = w.innerWidth || e.clientWidth || g.clientWidth,
-        y = w.innerHeight || e.clientHeight || g.clientHeight;
+        x = e.clientWidth || g.clientWidth,
+        y = e.clientHeight || g.clientHeight;
     return {
         width: x,
         height: y
