a6e3a00f5b1e4367222cbdfe63c9cb124e54dd66
filipnathanel <nathanel@o2.pl>
2016-08-08 16:03:45 +0200

    add automation events and layout

--- app/scripts/automation.js
+++ app/scripts/automation.js
@@ -1,4 +1,5 @@
 import * as utils from './utils/utils';
+import Globals from './globals';
 import XYController from './xyController/xyController';
 
 export default class Automation {
@@ -7,14 +8,75 @@ export default class Automation {
 
 		this.$automation = utils.getEl(automation);
 
-		this.xController = new XYController('#x-controller');
+		this.xController = new XYController('#x-controller', {
+			invert: true
+		});
 		this.yController = new XYController('#y-controller');
 		this.rotationController = new XYController('#rotation-controller');
 
+		this.controllers =[
+			this.xController,
+			this.yController,
+			this.rotationController
+		];
+
+		this.disabledHeight = 50;
+		this.activeController = null
+
+		this.init();
+
+	}
+
+	init(){
+		this.initEvents();
+		this.layout();
+	}
+
+	initEvents(){
+
+		this.controllers.forEach((controller, i) => {
+			utils.getEl('.control--enable', controller.$el).addEventListener('click', (e)=>{
+				// this.setActiveController(i);
+			});
+
+		});
+
+		Globals.onResize.add(() => {
+			this.controllers.forEach((controller) => {
+				// controller.pathData.redraw();
+			})
+		})
 	}
 
 	layout(){
 
+		var scanner = utils.getEl('.scanner');
+		var maxHeight = Globals.viewport.height - scanner.clientHeight;
+
+		var controllerMaxHeight = maxHeight - (this.controllers.length-1) * this.disabledHeight;
+
+		// set initially active controller
+		if ( !this.activeController ){
+			this.activeController = 0;
+		}
+
+	}
+
+	setActiveController(i){
+
+		if (typeof i === 'number'){
+			this.activeController = i;
+		}
+
+		this.controllers.forEach((controller, i) => {
+			if (this.activeController === i){
+				controller.$el.classList.add('active');
+				controller.$el.classList.remove('disabled');
+			}else{
+				controller.$el.classList.add('disabled');
+				controller.$el.classList.remove('active');
+			}
+		});
 	}
 
 	getValueAtPercent(percent){
